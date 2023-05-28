7c4cd4b7cecee1cb2875b7597a10f5601f3bdfc3
Forthtilliath <forth.developer@gmail.com>
2023-05-28 14:07:21 +0200

    refactor: move logic code to routes/api

--- src/lib/api/player.ts
+++ src/lib/api/player.ts
@@ -1,101 +1,31 @@
-import { transposeArrays } from "~/utils/methodes/array";
-import { setMonth } from "~/utils/methodes/date";
 import { prisma } from "~/utils/prisma/db";
 
-export async function getPlayerPremium() {
-  const res = await prisma.user.groupBy({
-    by: ["premium"],
-    _count: {
-      id: true,
+export async function getCountPremium(value = true) {
+  return prisma.user.count({
+    where: {
+      premium: value,
     },
   });
-
-  const label = (premium: boolean) => (premium ? "premium" : "gratuit");
-
-  return res.reduce(
-    (obj, { premium, _count }) =>
-      Object.assign(obj, { [label(premium)]: _count.id }),
-    {}
-  );
 }
 
-/**
- * @returns [free,premium]
- */
-async function getConnexionsBetween(from: Date, to = new Date()) {
-  const res = await prisma.user.groupBy({
-    by: ["premium"],
-    _count: {
-      id: true,
-    },
+export async function getCountConnexionFrom(from: Date, isPremium: boolean) {
+  return prisma.user.count({
     where: {
+      premium: isPremium,
       lastConnexion: {
         gt: from,
-        lte: to,
       },
     },
   });
-
-  return res.map((item) => item._count.id);
 }
 
-export async function getActives() {
-  const actives = await Promise.all([
-    getConnexionsBetween(setMonth(-1)),
-    getConnexionsBetween(setMonth(-3)),
-    getConnexionsBetween(setMonth(-6)),
-    getConnexionsBetween(setMonth(-12)),
-  ]);
-  const [free, premium] = transposeArrays(actives);
-
-  return [
-    {
-      name: "Gratuit",
-      data: free,
-    },
-    {
-      name: "Premium",
-      data: premium,
-    },
-  ] satisfies Chart.ColumnData;
-}
-
-/**
- * @returns [free,premium]
- */
-async function getConnexionsBefore(to = new Date()) {
-  const res = await prisma.user.groupBy({
-    by: ["premium"],
-    _count: {
-      id: true,
-    },
+export async function getCountConnexionTo(to: Date, isPremium: boolean) {
+  return prisma.user.count({
     where: {
+      premium: isPremium,
       lastConnexion: {
         lt: to,
       },
     },
   });
-
-  return res.map((item) => item._count.id);
-}
-
-export async function getInactives() {
-  const inactives = await Promise.all([
-    getConnexionsBefore(setMonth(-1)),
-    getConnexionsBefore(setMonth(-3)),
-    getConnexionsBefore(setMonth(-6)),
-    getConnexionsBefore(setMonth(-12)),
-  ]);
-  const [free, premium] = transposeArrays(inactives);
-
-  return [
-    {
-      name: "Gratuit",
-      data: free,
-    },
-    {
-      name: "Premium",
-      data: premium,
-    },
-  ] satisfies Chart.ColumnData;
 }
--- src/routes/api/player.ts
+++ src/routes/api/player.ts
@@ -1,26 +1,74 @@
 import { APIEvent, json } from "solid-start/api";
 import * as player from "~/lib/api/player";
+import { setMonth } from "~/utils/methodes/date";
 import { getSearchParam } from "~/utils/methodes/string";
 
 export async function GET({ request }: APIEvent) {
   const params = getSearchParam(request.url, "q");
   switch (params) {
     case "premiums": {
-      const premiums = await player.getPlayerPremium();
-      return json(premiums);
+      return json({
+        premium: await player.getCountPremium(true),
+        free: await player.getCountPremium(false),
+      });
     }
     // case "lastconnexions": {
     //   const lastconnexion = await player.getLastConnexions();
     //   return json(lastconnexion);
     // }
+
     case "actives": {
-      const actives = await player.getActives();
-      return json(actives);
+      const premium = await Promise.all([
+        player.getCountConnexionFrom(setMonth(-1), true),
+        player.getCountConnexionFrom(setMonth(-3), true),
+        player.getCountConnexionFrom(setMonth(-6), true),
+        player.getCountConnexionFrom(setMonth(-12), true),
+      ]);
+      const free = await Promise.all([
+        player.getCountConnexionFrom(setMonth(-1), false),
+        player.getCountConnexionFrom(setMonth(-3), false),
+        player.getCountConnexionFrom(setMonth(-6), false),
+        player.getCountConnexionFrom(setMonth(-12), false),
+      ]);
+
+      return json([
+        {
+          name: "Gratuit",
+          data: free,
+        },
+        {
+          name: "Premium",
+          data: premium,
+        },
+      ]);
     }
+
     case "inactives": {
-      const inactives = await player.getInactives();
-      return json(inactives);
+      const premium = await Promise.all([
+        player.getCountConnexionTo(setMonth(-1), true),
+        player.getCountConnexionTo(setMonth(-3), true),
+        player.getCountConnexionTo(setMonth(-6), true),
+        player.getCountConnexionTo(setMonth(-12), true),
+      ]);
+      const free = await Promise.all([
+        player.getCountConnexionTo(setMonth(-1), false),
+        player.getCountConnexionTo(setMonth(-3), false),
+        player.getCountConnexionTo(setMonth(-6), false),
+        player.getCountConnexionTo(setMonth(-12), false),
+      ]);
+
+      return json([
+        {
+          name: "Gratuit",
+          data: free,
+        },
+        {
+          name: "Premium",
+          data: premium,
+        },
+      ]);
     }
+
     case "connected": {
       return json({
         lundi: 45,
@@ -32,12 +80,14 @@ export async function GET({ request }: APIEvent) {
         dimanche: 124,
       });
     }
+
     case "satisfaction": {
       return json({
         satisfait: 1831,
         insatisfait: 102,
       });
     }
+
     default:
       return json({ error: "Not found" });
   }
