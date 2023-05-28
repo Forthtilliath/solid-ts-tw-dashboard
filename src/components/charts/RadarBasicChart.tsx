6e7f1aecf3683eaafc499a76b04d97cc39518970
Forthtilliath <forth.developer@gmail.com>
2023-05-28 14:11:06 +0200

    fix: options

--- src/components/charts/RadarBasicChart.tsx
+++ src/components/charts/RadarBasicChart.tsx
@@ -3,6 +3,7 @@ import { SolidApexCharts } from "solid-apexcharts";
 import { defaultOptions } from "./chart.options";
 import { mergeDeep } from "~/utils/methodes/object";
 import { createEffect } from "solid-js";
+import { isString } from "~/utils/methodes/string";
 
 type ApexChartProps = Parameters<typeof SolidApexCharts>[0];
 type ApexChartOptions = ApexChartProps["options"];
@@ -10,32 +11,39 @@ type ApexChartSeries = ApexChartProps["series"];
 
 type Props = {
   title: string;
+  subTitle?: string;
   labels: string[];
   data: Chart.RadarData;
-  colors?: string[];
+  colors?: string | string[];
   bgColors?: string[];
   options?: ApexChartOptions;
 };
 
 export default function RadarBasicChart(props: Props) {
+  let arrColor = isString(props.colors)
+    ? Array(props.labels.length).fill(props.colors)
+    : props.colors;
+
   const [options] = createStore(
     mergeDeep(
       structuredClone(defaultOptions),
       {
         chart: {
           type: "radar",
+          height: 500,
         },
         dataLabels: {
           enabled: true,
         },
         title: { text: props.title },
+        subtitle: { text: props.subTitle },
         colors: props.bgColors,
         labels: props.labels,
-        xaxis: { categories: props.labels },
-        fill: { opacity: 0.8 },
-        yaxis: {
-          labels: { style: { colors: props.colors } },
+        xaxis: {
+          categories: props.labels,
+          labels: { style: { colors: arrColor } },
         },
+        fill: { opacity: 0.8 },
       },
       props.options
     )
