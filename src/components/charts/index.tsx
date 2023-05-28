9ac3a0d1029cebd2a5ebcaf2ab2f8bbfbdb5b887
Forthtilliath <forth.developer@gmail.com>
2023-05-28 14:04:42 +0200

    refactor: remove Chart

--- src/components/charts/index.tsx
+++ src/components/charts/index.tsx
@@ -2,10 +2,6 @@ import { unstable_clientOnly } from "solid-start";
 
 // !window is not defined
 
-export const SolidApexCharts = unstable_clientOnly(
-  () => import("~/components/charts/Chart")
-);
-
 export const PieChart = unstable_clientOnly(
   () => import("~/components/charts/PieChart")
 );
