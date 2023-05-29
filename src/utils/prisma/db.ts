79f2b5af0f38ed8d643f1617676b3f412e79d34e
Forthtilliath <forth.developer@gmail.com>
2023-05-29 23:42:06 +0200

    refactor: add extends methods to prisma

--- src/utils/prisma/db.ts
+++ src/utils/prisma/db.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { createGames, createPlayedGames, createUsers, generateData } from "./generateData";
+import {
+  createGames,
+  createPlayedGames,
+  createUsers,
+  generateData,
+} from "./generateData";
 
 export const prisma = new PrismaClient({
   log: [
@@ -11,14 +16,14 @@ export const prisma = new PrismaClient({
       emit: "stdout",
       level: "error",
     },
-    {
-      emit: "stdout",
-      level: "info",
-    },
-    {
-      emit: "stdout",
-      level: "warn",
-    },
+    // {
+    //   emit: "stdout",
+    //   level: "info",
+    // },
+    // {
+    //   emit: "stdout",
+    //   level: "warn",
+    // },
   ],
 }).$extends({
   model: {
@@ -30,20 +35,52 @@ export const prisma = new PrismaClient({
         }
         return user;
       },
-      getAllIds: async () => {
-        return await prisma.user.findMany({ select: { id: true } });
+      // getAllIds: async () => {
+      //   return await prisma.user.findMany({ select: { id: true } });
+      // },
+      createMany: async (newUsers: DB.UserCreation[]) => {
+        await prisma.$transaction([
+          ...newUsers.map((user) => prisma.user.create({ data: user })),
+        ]);
+      },
+      createManyIfNotExists: async (newUsers: DB.UserCreation[]) => {
+        await prisma.$transaction([
+          ...newUsers.map((user) =>
+            prisma.user.upsert({
+              where: { username: user.username },
+              create: user,
+              update: {},
+            })
+          ),
+        ]);
       },
     },
     game: {
-      getAllIds: async () => {
-        return await prisma.game.findMany({ select: { id: true } });
+      // getAllIds: async () => {
+      //   return await prisma.game.findMany({ select: { id: true } });
+      // },
+      createMany: async (newGames: DB.GameCreation[]) => {
+        await prisma.$transaction([
+          ...newGames.map((game) => prisma.game.create({ data: game })),
+        ]);
+      },
+      createManyIfNotExists: async (newGames: DB.GameCreation[]) => {
+        await prisma.$transaction([
+          ...newGames.map((game) =>
+            prisma.game.upsert({
+              where: { name: game.name },
+              update: {},
+              create: game,
+            })
+          ),
+        ]);
       },
     },
   },
 });
 
 prisma.$on("query", (e) => {
-  const params = JSON.parse(e.params)
+  const params = JSON.parse(e.params);
   const query = e.query.replace(/(\?)/g, () => params.shift());
   console.log("Query: " + query);
 });
@@ -51,6 +88,6 @@ prisma.$on("query", (e) => {
 // generateData({ users: 100, histories: 5000 });
 // generateData({ playedGames:5 });
 
-// await createGames();
-// await createUsers(1);
+await createGames();
+// await createUsers(20);
 // await createPlayedGames(2000)
