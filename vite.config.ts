9fd977e2eb9241b5c16fde3a7b87ea0446bb0744
Forthtilliath <forth.developer@gmail.com>
2023-05-30 20:54:51 +0200

    build: add vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,43 @@
 import solid from "solid-start/vite";
+import vercel from "solid-start-vercel";
+// import netlify from "solid-start-netlify";
 import { defineConfig } from "vite";
 
+// export default defineConfig({
+//   // plugins: [solid()],
+//   plugins: [{
+//   //   ...(await import('@mdx-js/rollup')).default({
+//   //     jsx: true,
+//   //     jsxImportSource: "solid-js",
+//   //     providerImportSource: "solid-mdx"
+//   //   }),
+//   //   enforce: "pre"
+//   // }),
+//   solid({
+//     // extensions: ['.md','.mdx']
+//     adapter:vercel()
+//   })
+// });
+
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      // adapter: netlify({edge:true})
+      adapter: vercel({ edge: true }),
+    }),
+  ],
 });
+
+// let options = {};
+// if (import.meta.env.PROD) {
+//   options = {
+//     adapter: vercel({ edge: true }),
+//   };
+// }
+
+// export default defineConfig({
+//   plugins: [solid(options)],
+// });
+// export default defineConfig({
+//   plugins: [solid()],
+// });
